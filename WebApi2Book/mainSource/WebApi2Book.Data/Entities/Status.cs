6619ed4d0919146e0c2b67cd6f9be18f631c3986
ttto <tto@tegna.com>
2016-02-03 23:01:35 -0500

    check in

---
+++ WebApi2Book/mainSource/WebApi2Book.Data/Entities/Status.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace WebApi2Book.Data.Entities
+{
+    public class Status
+    {
+        public virtual long StatusId { get; set; }
+        public virtual string Name { get; set; }
+        public virtual int Ordinal { get; set; }
+        public virtual byte[] Version { get; set; }
+    }
+}
---
+++ WebApi2Book/mainSource/WebApi2Book.Data/Entities/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi2Book.Data.Entities
+{
+    public class Task
+    {
+        private readonly IList<User> _users;
+        public virtual long TaskId { get; set; }
+        public virtual string Subject { get; set; }
+        public virtual DateTime? StartDate { get; set; }
+        public virtual DateTime? DueDate { get; set; }
+        public virtual DateTime? CompletedDate { get; set; }
+        public virtual Status Status { get; set; }
+        public virtual DateTime CreateDate { get; set; }
+
+        public virtual IList<User> Users
+        {
+            get { return _users; }
+        }
+
+        public virtual User CreatedBy { get; set; }
+    }
+}
